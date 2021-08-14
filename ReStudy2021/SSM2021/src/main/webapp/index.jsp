746be4c7acd45f95a1e678da6d2a4d4da3145cdf
jackyjinchen <jackyjinchen@163.com>
2021-08-15 01:57:08 +0800

    修改重新放2021文件

--- ReStudy2021/SSM2021/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-<a href="student">查询所有学生信息</a>
-</body>
-</html>
--- ReStudy2021/Spring/src/com/jacky/AOP/UserDao.java
+++
@@ -1,6 +0,0 @@
-package com.jacky.AOP;
-
-public interface UserDao {
-    public int add(int a, int b);
-    public String update(String id);
-}
